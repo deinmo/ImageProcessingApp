d647bae76ebbb608e934ba86cdc3ace8b2ecc4c9
deinmo <deinmo23@gmail.com>
2020-03-16 09:19:46 +0100

    Image processing basics

--- app/src/main/java/jan/org/ng/studentapplication/MainActivity.java
+++ app/src/main/java/jan/org/ng/studentapplication/MainActivity.java
@@ -3,33 +3,43 @@ package jan.org.ng.studentapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Button firstbutton, secondbutton;
+    HashMap myhashmap = new HashMap();
+    private RecyclerView mrecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        firstbutton = (Button)findViewById(R.id.Aboutalc);
-        secondbutton = (Button)findViewById(R.id.myprofile);
 
-        firstbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent1 = new Intent(MainActivity.this,AboutALC.class);
-                startActivity(intent1);
-            }
-        });
-        secondbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent1 = new Intent(MainActivity.this,MyProfile.class);
-                startActivity(intent1);
-            }
-        });
+        mrecycler = (RecyclerView)findViewById(R.id.myrecycler);
+        mrecycler.setHasFixedSize(true);
+        mrecycler.setLayoutManager(new LinearLayoutManager(this));
+        ImageList [] Imagelist = new ImageList[]{
+                new ImageList(R.mipmap.firstim,"First Image"),
+                new ImageList(R.mipmap.secondim,"Second Image"),
+                new ImageList(R.mipmap.thirdim,"Third Image"),
+                new ImageList(R.mipmap.fourthim,"Fourth Image"),
+                new ImageList(R.mipmap.fifthim,"Fifth Image"),
+                new ImageList(R.mipmap.sixthim,"Sixth Image"),
+        };
+
+
+
+
+        ImageAdapter myadapter = new ImageAdapter(MainActivity.this,Imagelist);
+        mrecycler.setAdapter(myadapter);
     }
 
 }
--- app/src/main/java/jan/org/ng/studentapplication/Questionairre.java
+++ app/src/main/java/jan/org/ng/studentapplication/Questionairre.java
@@ -5,19 +5,15 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MyProfile extends AppCompatActivity {
+public class Questionairre extends AppCompatActivity {
     private ImageView myimageview;
     private TextView first, second, third, fourth, fifth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_profile);
+        setContentView(R.layout.activity_questionairre);
         myimageview = (ImageView)findViewById(R.id.imageView);
-        first = (TextView) findViewById(R.id.textView1);
-        second = (TextView) findViewById(R.id.textView2);
-        third = (TextView)findViewById(R.id.textView3);
-        fourth = (TextView) findViewById(R.id.textView4);
-        fifth = (TextView)findViewById(R.id.textView5);
+
     }
 }
